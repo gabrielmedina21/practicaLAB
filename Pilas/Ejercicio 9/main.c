93deae1be3609c4e814615c701ebea580bd68b6f
gabrielmedina21 <85813624+gabrielmedina21@users.noreply.github.com>
2023-07-08 15:57:15 -0300

    main.c

---
+++ Pilas/Ejercicio 9/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include "pila.h"
+
+int main()
+{
+
+/** DECLARACIÓN DE VARIABLES **/
+    Pila a;
+    Pila b;
+    Pila aux;
+    char continuar = 's';
+
+    inicpila(&a);
+    inicpila(&b);
+    inicpila(&aux);
+
+/* ---------------------------------------------- */
+
+    do{
+        system("cls");
+
+        printf("\t\t\t EJERCICIO 9 - PILAS \n\n");
+
+        printf(" CARGANDO PILA A... \n\n");
+        leer(&a);
+        printf("\n %cQuieres continuar cargando la pila? s=SI / n=NO ");
+        fflush(stdin);
+        continuar = getch();
+
+    }while(continuar == 's' || continuar == 'S');
+
+    do{
+        system("cls");
+
+        printf("\t\t\t EJERCICIO 9 - PILAS \n\n");
+
+        printf(" CARGANDO PILA B... \n\n");
+        leer(&b);
+        printf("\n %cQuieres continuar cargando la pila? s=SI / n=NO ");
+        fflush(stdin);
+        continuar = getch();
+
+    }while(continuar == 's' || continuar == 'S');
+
+    system("cls");
+
+    printf(" PILA A CARGADA \n");
+    mostrar(&a);
+    printf(" PILA B CARGADA \n");
+    mostrar(&b);
+
+    while((!pilavacia(&a)) && (!pilavacia(&b))){
+        apilar(&aux , desapilar(&a));
+        apilar(&aux , desapilar(&b));
+    }
+
+    if((pilavacia(&a)) && (pilavacia(&b))){
+        printf(" La cantidad de elementos de ambas pilas son iguales. \n\n");
+    }else{
+        printf(" La cantidad de elementos de ambas pilas son distintos. \n\n");
+    }
+
+    return 0;
+}
