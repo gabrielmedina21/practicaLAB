da424f540f1fb2ae94e5850fcc89f8d2841fc9cf
gabrielmedina21 <85813624+gabrielmedina21@users.noreply.github.com>
2023-07-08 06:34:15 -0300

    Add files via upload

---
+++ Pilas/Ejercicio 4/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include "pila.h"
+
+int main()
+{
+    /** DECLARACION DE VARIABLES **/
+    Pila origen;
+    Pila destino;
+    Pila auxDestino;
+    char continuar = 's';
+
+    inicpila(&origen);
+    inicpila(&destino);
+    inicpila(&auxDestino);
+
+    /* ---------------------------------------------------- */
+
+    do{
+       system("cls");
+
+       printf("\t\t\t EJERCICIO 4 - PILAS \n\n");
+
+       printf(" CARGANDO PILA ORIGEN.... \n\n");
+
+       leer(&origen);
+
+       printf("\n %cQuieres continuar cargando la pila? s=SI / n=NO ");
+       fflush(stdin);
+       continuar = getch();
+
+    }while(continuar == 's' || continuar == 'S');
+
+    system("cls");
+
+    printf(" PILA ORIGEN CARGADA \n");
+    mostrar(&origen);
+
+    while(!pilavacia(&origen)){
+        apilar(&auxDestino , desapilar(&origen));
+    }
+
+    while(!pilavacia(&auxDestino)){
+        apilar(&destino , desapilar(&auxDestino));
+    }
+
+    printf(" PILA DESTINO \n");
+    mostrar(&destino);
+
+    return 0;
+}
