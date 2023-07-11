8d58b8240d2dbe50d7d7081c0e375882793ca1e1
gabrielmedina21 <85813624+gabrielmedina21@users.noreply.github.com>
2023-07-11 07:23:50 -0300

    Add files via upload

---
+++ Pilas Avanzado/Ejercicio 7/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "pila.h"
+
+int main()
+{
+/** DECLARACIÓN DE VARIABLES **/
+
+    Pila dada;
+    Pila auxDada;
+    Pila ordenada;
+    int menor = 0;
+    int i;
+
+    inicpila(&dada);
+    inicpila(&auxDada);
+    inicpila(&ordenada);
+
+/* ------------------------------------- */
+    printf("\t\t\t EJERCICIO 6 - PILAS AVANZADO \n\n");
+
+    srand(time(NULL));
+
+    for(i=0 ; i<10 ; i++){
+        apilar(&dada , rand() %50);
+    }
+
+    printf(" PILA CARGADA ALEATORIAMENTE \n");
+    mostrar(&dada);
+
+    while(!pilavacia(&dada)){
+        menor = desapilar(&dada);
+        while(!pilavacia(&dada)){
+            if(tope(&dada) < menor){
+                apilar(&auxDada , menor);
+                menor = desapilar(&dada);
+            }else{
+                apilar(&auxDada , desapilar(&dada));
+            }
+        }
+
+         while(!pilavacia(&auxDada)){
+            apilar(&dada , desapilar(&auxDada));
+        }
+
+        apilar(&ordenada , menor);
+    }
+
+    printf(" PILA ORDENADA POR SELECCI%cN \n" , 224);
+    mostrar(&ordenada);
+
+    return 0;
+}
