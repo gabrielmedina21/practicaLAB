dd77d9b39256460a51a0a8248ac6348a62dbedf0
gabrielmedina21 <85813624+gabrielmedina21@users.noreply.github.com>
2023-07-10 21:47:42 -0300

    Create main.c

---
+++ Pilas Avanzado/Ejercicio 4/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include "pila.h"
+
+int main()
+{
+/** DECLARACIÓN DE VARIABLES **/
+
+    Pila dada;
+    Pila menor;
+    Pila auxDada;
+    int i;
+
+    inicpila(&dada);
+    inicpila(&menor);
+    inicpila(&auxDada);
+
+/* ------------------------------------- */
+
+    printf(" EJERCICIO 4 - PILAS AVANZADO \n\n");
+
+    srand(time(NULL));
+
+    for(i=0 ; i<10 ; i++){
+        apilar(&dada , rand() %100);
+    }
+
+    printf(" PILA CARGADA ALEATORIAMENTE \n");
+    mostrar(&dada);
+
+    apilar(&menor , desapilar(&dada));
+
+    if(!pilavacia(&dada)){
+        while(!pilavacia(&dada)){
+            if(tope(&dada) < tope(&menor)){
+                apilar(&auxDada , desapilar(&menor));
+                apilar(&menor , desapilar(&dada));
+            }else{
+                apilar(&auxDada , desapilar(&dada));
+            }
+        }
+    }
+
+    while(!pilavacia(&auxDada)){
+        apilar(&dada , desapilar(&auxDada));
+    }
+
+    printf(" El menor de la pila es: %d \n\n" , tope(&menor));
+
+    return 0;
+}
