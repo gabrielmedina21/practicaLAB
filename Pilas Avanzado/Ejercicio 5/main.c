5c5d6c6572abbf67681d545769f57a79540c5de9
gabrielmedina21 <85813624+gabrielmedina21@users.noreply.github.com>
2023-07-10 22:37:32 -0300

    Create main.c

---
+++ Pilas Avanzado/Ejercicio 5/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pila.h"
+
+int main()
+{
+/** DECLARACIÓN DE VARIBALES **/
+
+    Pila dada;
+    Pila auxDada;
+    Pila numAInsertar;
+
+    inicpila(&dada);
+    inicpila(&auxDada);
+    inicpila(&numAInsertar);
+
+/* ------------------------------------- */
+    printf(" EJERCICIO 5 - PILAS AVANZADO \n\n");
+
+    apilar(&dada , 5);
+    apilar(&dada , 4);
+    apilar(&dada , 3);
+    apilar(&dada , 2);
+    apilar(&dada , 1);
+
+    printf(" PILA CARGADA ORDENADA \n");
+    mostrar(&dada);
+
+    printf(" Ingresar n%cmero a insertar: \n" , 163);
+    leer(&numAInsertar);
+
+    while((!pilavacia(&dada)) && (tope(&dada) < tope(&numAInsertar))){
+        apilar(&auxDada , desapilar(&dada));
+    }
+
+    apilar(&dada , desapilar(&numAInsertar));
+
+    while(!pilavacia(&auxDada)){
+        apilar(&dada , desapilar(&auxDada));
+    }
+
+    printf("\n");
+    system("pause");
+    system("cls");
+
+    printf("\n PILA ACTUALIZADA \n");
+    mostrar(&dada);
+
+    return 0;
+}
